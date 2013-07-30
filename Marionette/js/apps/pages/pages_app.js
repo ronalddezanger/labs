8a38821d4aaade63351634a414a146c1d7c9e9e9
Ronald de Zanger <r.dezanger@greenvalley.nl>
2013-07-30 23:07:37 +0200

    added

--- Marionette/js/apps/pages/pages_app.js
+++ Marionette/js/apps/pages/pages_app.js
@@ -1,15 +1,24 @@
 App.module('PagesApp', function(PagesApp, App, Backbone, Marionette, $, _) {
     PagesApp.Router = Marionette.AppRouter.extend({
         appRoutes: {
-            "pages": "listPages"
+            "pages": "listPages",
+            "pages/:id": "showPage"
         }
     });
 
     var API = {
         listPages: function() {
-            console.log("triggered router");
+            PagesApp.List.Controller.listPages();
+        },
+        showPage: function(id) {
+            PagesApp.Show.Controller.showPage();
         }
     };
+
+    App.on("pages:list", function() {
+        App.navigate("pages");
+        API.listPages();
+    });
 
     App.addInitializer(function() {
         new PagesApp.Router({ controller: API });
--- Marionette/js/apps/pages/show/show_controller.js
+++ Marionette/js/apps/pages/show/show_controller.js
@@ -1,6 +1,8 @@
 App.module('PagesApp.Show', function(Show, App, Backbone, Marionette, $, _) {
     Show.Controller = {
-        showPage: function(model) {
+        showPage: function(id) {
+			var pages = App.request("pages:entities");
+			var model = pages.get(id);
             var pageView = new Show.Page({
                 model: model
             });
