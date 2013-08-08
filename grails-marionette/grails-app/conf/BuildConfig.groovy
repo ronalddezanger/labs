fe01818895e339fac15139dcd10e09f4b7f90b35
ronalddezanger <ronald@ronalddezanger.nl>
2013-08-08 11:30:58 +0200

    added travis and sane tomcat plugin

--- grails-marionette/grails-app/conf/BuildConfig.groovy
+++ grails-marionette/grails-app/conf/BuildConfig.groovy
@@ -53,7 +53,7 @@ grails.project.dependency.resolution = {
 
     plugins {
         // plugins for the build system only
-        build ":tomcat:7.0.41"
+        build ":tomcat:2.2.4"
 
         // plugins for the compile step
         compile ":scaffolding:2.0.0.RC1"
